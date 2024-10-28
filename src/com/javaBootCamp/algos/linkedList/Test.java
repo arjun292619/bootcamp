27bee31915440a8671bd3a69eebe885821e07db7
arjun292619 <j.c.arjunreddy@gmail.com>
2024-10-28 11:42:32 -0700

    linked list insertio operatons

--- src/com/javaBootCamp/algos/linkedList/Test.java
+++ src/com/javaBootCamp/algos/linkedList/Test.java
@@ -13,6 +13,21 @@ public class Test {
         test.printListIterative(head);
         System.out.println();
         test.printListRecursive(head);
+        Node list1 = test.insertAtBegin(null, 5);
+        list1 = test.insertAtBegin(list1, 15);
+        list1 = test.insertAtBegin(list1, 25);
+        list1 = test.insertAtBegin(list1, 35);
+        list1 = test.insertAtBegin(list1, 45);
+//        System.out.println();
+        test.printListRecursive(list1);
+
+        Node list2 = test.insertAtEnd(null, 6);
+        list2 = test.insertAtEnd(list2, 16);
+        list2 = test.insertAtEnd(list2, 26);
+        list2 = test.insertAtEnd(list2, 36);
+        list2 = test.insertAtEnd(list2, 46);
+        test.printListIterative(list2);
+
     }
 
     public void printListIterative(Node head) {
@@ -22,13 +37,32 @@ public class Test {
 
             current = current.next;
         }
+        System.out.println();
     }
 
     public void printListRecursive(Node head) {
         if (head == null) {
+            System.out.println();
             return;
         }
         System.out.print(head.data + " ");
         printListRecursive(head.next);
+    }
+
+    public Node insertAtBegin(Node node, int x) {
+        Node newhead = new Node(x);
+        newhead.next = node;
+        return newhead;
+    }
+
+    public Node insertAtEnd(Node node, int x) {
+        Node newNode = new Node(x);
+        if (node == null) return newNode;
+        Node temp = node;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return node;
     }
 }

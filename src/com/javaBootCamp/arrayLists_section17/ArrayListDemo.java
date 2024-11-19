7443abe6c89a09d25145c5e1e6eb571fee83e439
arjun292619 <j.c.arjunreddy@gmail.com>
2024-11-19 11:18:21 -0800

    ListArray demo

--- src/com/javaBootCamp/arrayLists_section17/ArrayListDemo.java
+++ src/com/javaBootCamp/arrayLists_section17/ArrayListDemo.java
@@ -1,7 +1,29 @@
 package com.javaBootCamp.arrayLists_section17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDemo {
     public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        List<Character> characters = new ArrayList<>();
+        List<String> countries = new ArrayList<>();
+        var doubles = new ArrayList<Double>();
+        System.out.println(countries.size());
+        countries.add("India");
+        countries.add("Canada");
+        countries.add("USA");
+        countries.add("Germany");
+        countries.add("India");
+        countries.add("Mexico");
+        countries.add("UK");
+        System.out.println(countries);
+        countries.add(2, "France");
+        System.out.println(countries);
+        countries.set(4, "Spain");
+        countries.remove(5);
+        countries.remove("France");
+        System.out.println(countries);
 
     }
 }

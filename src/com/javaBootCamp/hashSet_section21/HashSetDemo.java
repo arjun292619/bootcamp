7feb4d35bdd2de7a185849e58f327830d894b697
arjun292619 <j.c.arjunreddy@gmail.com>
2024-11-22 23:42:13 -0800

    Hashset Demo Iteration with iterator

--- src/com/javaBootCamp/hashSet_section21/HashSetDemo.java
+++ src/com/javaBootCamp/hashSet_section21/HashSetDemo.java
@@ -1,6 +1,7 @@
 package com.javaBootCamp.hashSet_section21;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class HashSetDemo {
@@ -12,13 +13,27 @@ public class HashSetDemo {
         visitedCountries.add("Iceland");
         visitedCountries.add("UK");
         visitedCountries.add("Japan");
-        visitedCountries.add("Japan");
+        visitedCountries.add("South Korea");
+        visitedCountries.add("Russia");
+        visitedCountries.add("Brazil");
+        visitedCountries.add("Denmark");
         System.out.println(visitedCountries.size());
         System.out.println(visitedCountries);
 
         boolean isFrance = visitedCountries.contains("France");
         boolean isSingapore = visitedCountries.contains("Singapore");
         boolean delJapan = visitedCountries.remove("Japan");
+        System.out.println(visitedCountries);
+
+        Iterator<String> iterator = visitedCountries.iterator();
+        while (iterator.hasNext()) {
+            String country = iterator.next();
+            System.out.println(country.toUpperCase());
+            if (country.contains("u")) {
+                iterator.remove();
+            }
+        }
+
         System.out.println(visitedCountries);
 
     }

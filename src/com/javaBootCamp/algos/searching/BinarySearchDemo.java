cd797291554736fee408354b6fe21761be3d77df
arjun292619 <j.c.arjunreddy@gmail.com>
2024-12-04 11:25:45 -0800

    Recursive binary search

--- src/com/javaBootCamp/algos/searching/BinarySearchDemo.java
+++ src/com/javaBootCamp/algos/searching/BinarySearchDemo.java
@@ -1,11 +1,18 @@
 package com.javaBootCamp.algos.searching;
 
+import java.rmi.dgc.VMID;
+
 public class BinarySearchDemo {
     public static void main(String[] args) {
         BinarySearchDemo binarySearchDemo = new BinarySearchDemo();
         int[] sample1 = {10, 20, 30, 40, 50, 60};
+//        Iterative Binary Search
         int index1 = binarySearchDemo.bSearchIterative(sample1, 20);
         System.out.println("result found at index: " + index1);
+
+        //        Recursive Binary Search
+        int index2 = binarySearchDemo.bSearchIterative(sample1, 40);
+        System.out.println("result found at index: " + index2);
     }
 
     public int bSearchIterative(int[] arr, int key) {
@@ -21,5 +28,17 @@ public class BinarySearchDemo {
             }
         }
         return mid;
+    }
+
+    public int bSearchRecursive(int[] arr, int key, int low, int high) {
+        if (low > high) return -1;
+        int mid = (high + low) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] > key) {
+            return bSearchRecursive(arr, key, low, mid - 1);
+        } else {
+            return bSearchRecursive(arr, key, mid + 1, high);
+        }
     }
 }

74158b87c60d383ae7a2979e8c78158c2c452990
arjun292619 <j.c.arjunreddy@gmail.com>
2024-12-04 15:50:13 -0800

    Stream Creation Demo

--- src/com/javaBootCamp/streams_section26/StreamCreationDemo.java
+++ src/com/javaBootCamp/streams_section26/StreamCreationDemo.java
@@ -1,7 +1,9 @@
 package com.javaBootCamp.streams_section26;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class StreamCreationDemo {
@@ -13,5 +15,25 @@ public class StreamCreationDemo {
         System.out.println("-".repeat(30));
         Stream<String> parallelStream = departmentList.parallelStream();
         parallelStream.forEach(System.out::println);
+        System.out.println("-".repeat(30));
+        departmentList.stream().parallel().forEach(System.out::println);
+
+        String[] cities = {"Chicago", "Melbourne", "Paris", "London", "Tokyo", "Singapore", "Kuwait", "Portland", "Seattle", "Dallas", "Houston"};
+//        Convert an array to stream
+        System.out.println("-".repeat(30));
+        Stream<String> cityStream = Arrays.stream(cities);
+        cityStream.forEach(System.out::println);
+
+//        create and empty stream
+        Stream<String> emptyStream = Stream.empty();
+
+//        generate infinite stream data
+        System.out.println("-".repeat(30));
+        Stream.generate(new Random()::nextInt).limit(25).forEach(System.out::println);
+        System.out.println("-".repeat(30));
+
+        //        generate infinite stream data with iterate
+        Stream.iterate(1, n -> n * 2).limit(15).forEach(System.out::println);
+
     }
 }

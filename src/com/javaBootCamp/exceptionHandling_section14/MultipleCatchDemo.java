184b289932b4f2d15e44fa13d8ee6334941e49e2
arjun292619 <j.c.arjunreddy@gmail.com>
2024-12-16 13:15:53 -0800

    Try with resources demo

--- src/com/javaBootCamp/exceptionHandling_section14/MultipleCatchDemo.java
+++ src/com/javaBootCamp/exceptionHandling_section14/MultipleCatchDemo.java
@@ -9,13 +9,15 @@ public class MultipleCatchDemo {
         String input = "Hello";
         try {
             logger.info(input.toUpperCase());
-            input = input.substring(0, 8);
+            input = input.substring(2, 8);
         } catch (NullPointerException nex) {
             logger.severe("Null pointer exception has occured. Please check the variable");
         } catch (StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException ex) {
             logger.severe("String index is out of bounds. Please check you index");
         } catch (Exception ex) {
             logger.severe("a general exception has occured");
+        } finally {
+            System.out.println("this block executes all the time with or without exception");
         }
         System.out.println("Program continues");
     }

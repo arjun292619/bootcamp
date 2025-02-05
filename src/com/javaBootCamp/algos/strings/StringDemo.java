1aa38943c08572caecf56598c739b0c67563c8dc
arjun292619 <j.c.arjunreddy@gmail.com>
2025-02-05 11:32:56 -0800

    BigDecimalDemo

---
+++ src/com/javaBootCamp/algos/strings/StringDemo.java
@@ -0,0 +1,9 @@
+package com.javaBootCamp.algos.strings;
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/com/javaBootCamp/nutsNBolts/bigdecimal/BigDecimalDemo.java
+++ src/com/javaBootCamp/nutsNBolts/bigdecimal/BigDecimalDemo.java
@@ -1,7 +1,47 @@
 package com.javaBootCamp.nutsNBolts.bigdecimal;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class BigDecimalDemo {
     public static void main(String[] args) {
+        double policyAmount = 100_000_000;
+        int beneficiaries = 3;
+        float percentageFloat = 1.0f / 3;
+        double percentageDouble = 1.0 / 3;
+        System.out.printf("Payout with float = %.2f%n", policyAmount * percentageFloat);
+        System.out.printf("Payout with double= %.2f%n", policyAmount * percentageDouble);
+
+        double totalDifferentialFloat = policyAmount - (policyAmount * percentageFloat * beneficiaries);
+        double totalDifferentialDbl = policyAmount - (policyAmount * percentageDouble * beneficiaries);
+
+        System.out.printf("totalDifferential with float = %.2f%n", totalDifferentialFloat);
+        System.out.printf("totalDifferential with double = %.2f%n", totalDifferentialDbl);
+
+        String[] decimalString = {"15.456", "8", "10000.000001", ".123896"};
+        Double[] decimallDbl = {15.456, 8d, 10000.000001, .123896};
+        BigDecimal[] bigDArray = new BigDecimal[decimalString.length];
+        Arrays.setAll(bigDArray, i -> new BigDecimal(decimalString[i]));
+        BigDecimal[] bigDbls = new BigDecimal[decimalString.length];
+        Arrays.setAll(bigDbls, i -> BigDecimal.valueOf(decimallDbl[i]));
+        System.out.printf("%14s %-15s %8s %s%n", "Value", "Unscaled value", "Scale", "Precision");
+//        String template = String.format("%15s %-15d %8d %d %n" );
+        System.out.println("-".repeat(50));
+        for (BigDecimal bd : bigDArray) {
+            System.out.printf("%15s %-15d %8d %d %n", bd, bd.unscaledValue(), bd.scale(), bd.precision());
+        }
+
+        System.out.printf("%14s %-15s %8s %s%n", "Value", "Unscaled value", "Scale", "Precision");
+//        String template = String.format("%15s %-15d %8d %d %n" );
+        System.out.println("-".repeat(50));
+        for (BigDecimal bd : bigDbls) {
+            System.out.printf("%15s %-15d %8d %d %n", bd, bd.unscaledValue(), bd.scale(), bd.precision());
+        }
+        System.out.println("-".repeat(50));
+        BigDecimal testVal1 = new BigDecimal("1.111111111222222244444333333355555556663333");
+        BigDecimal testVal2 = BigDecimal.valueOf(1.111111111222222244444333333355555556663333);
+        System.out.printf("%15s %-15d %8d %d %n", testVal1, testVal1.unscaledValue(), testVal1.scale(), testVal1.precision());
+        System.out.printf("%15s %-15d %8d %d %n", testVal2, testVal2.unscaledValue(), testVal2.scale(), testVal2.precision());
 
     }
 }

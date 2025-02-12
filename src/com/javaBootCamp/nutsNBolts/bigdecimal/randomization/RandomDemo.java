6661141c1a635252161e5d2b67426b99068f3a15
arjun292619 <j.c.arjunreddy@gmail.com>
2025-02-11 19:49:34 -0800

    random stream demo

--- src/com/javaBootCamp/nutsNBolts/bigdecimal/randomization/RandomDemo.java
+++ src/com/javaBootCamp/nutsNBolts/bigdecimal/randomization/RandomDemo.java
@@ -31,8 +31,8 @@ public class RandomDemo {
         System.out.println();
         System.out.println("-".repeat(30));
         System.out.println("Generating random character with range of 26 and low 65 & high 91 values");
+        Random r = new Random();
         for (int i = 0; i < 10; i++) {
-            Random r = new Random();
             System.out.printf("%1$d : %1$c%n", r.nextInt(65, 92));
         }
 
@@ -40,8 +40,22 @@ public class RandomDemo {
         System.out.println("-".repeat(30));
         System.out.println("Generating random character with range of 26 and low 65 & high 91 values");
         for (int i = 0; i < 10; i++) {
-            Random r = new Random();
-            System.out.printf("%1$d : %1$c%n", r.nextInt((int)'A', (int)'z'));
+            System.out.printf("%1$d : %1$c%n", r.nextInt((int) 'A', (int) 'z' + 1));
         }
+
+        System.out.println();
+        System.out.println("-".repeat(30));
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("%d%n", r.nextInt());
+        }
+        System.out.println("-".repeat(30));
+// creating a stream of random integers
+        r.ints().limit(5).forEach(System.out::println);
+        System.out.println("-".repeat(30));
+        System.out.println("random integer stream between a range");
+        r.ints(1, 21).limit(5).forEach(System.out::println);
+        System.out.println("-".repeat(30));
+        System.out.println("random double stream between a range with size method");
+        r.doubles(6, 1, 10).forEach(System.out::println);
     }
 }

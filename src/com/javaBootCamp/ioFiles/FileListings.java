89a970dad071094bb48024b1564181428941290d
arjun292619 <j.c.arjunreddy@gmail.com>
2025-03-19 13:53:05 -0700

    Walk and Find files and print and current dierctory details

--- src/com/javaBootCamp/ioFiles/FileListings.java
+++ src/com/javaBootCamp/ioFiles/FileListings.java
@@ -12,13 +12,53 @@ public class FileListings {
         System.out.println("-".repeat(30));
         Path currentPath = Path.of("");
         System.out.println("absolute path of cwd: " + currentPath.toAbsolutePath());
-        System.out.println("-".repeat(30));
+
+//        Using List to list the files and print the contents
+        System.out.println("-".repeat(20) + "using List" + "-".repeat(20));
+
         try (Stream<Path> paths = Files.list(currentPath)) {
             paths
                     .map(FileListings::listDir)
                     .forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        System.out.println("-".repeat(50));
+
+//        Using List to list the files and print the contents
+        System.out.println("-".repeat(20) + "using Walk" + "-".repeat(20));
+        try (var paths = Files.walk(currentPath, 2)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .map(FileListings::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("-".repeat(50));
+
+//        Using List to list the files and print the contents
+        System.out.println("-".repeat(20) + "Using Find" + "-".repeat(20));
+        try (var paths = Files.find(currentPath, Integer.MAX_VALUE, (p, attr) -> attr.isRegularFile() && attr.size() > 800)) {
+            paths
+                    .map(FileListings::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("-".repeat(50));
+
+//        Using List to list the files and print the contents
+        System.out.println("=".repeat(20) + "Using Directory Stream" + "=".repeat(20));
+        Path subPath = currentPath.resolve(".idea");
+        try (var pathsStream = Files.newDirectoryStream(subPath, "*.xml")) {
+            pathsStream
+                    .forEach(d -> System.out.println(FileListings.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 

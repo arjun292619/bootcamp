9b6351d634de770b1519be53f8eae92c3f456ce6
arjun292619 <j.c.arjunreddy@gmail.com>
2025-03-19 23:44:38 -0700

    file walker and size accumlation for dfs

--- src/com/javaBootCamp/ioFiles/FileWalker.java
+++ src/com/javaBootCamp/ioFiles/FileWalker.java
@@ -9,8 +9,8 @@ import java.util.Objects;
 
 public class FileWalker {
     public static void main(String[] args) {
-        Path startingPath = Path.of(".");
-        FileVisitor<Path> statsVisitor = new StatsVisitor();
+        Path startingPath = Path.of("..");
+        FileVisitor<Path> statsVisitor = new StatsVisitor(1);
         try {
             Files.walkFileTree(startingPath, statsVisitor);
         } catch (IOException e) {
@@ -22,11 +22,17 @@ public class FileWalker {
         private Path initialPath = null;
         private final Map<Path, Long> folderSizes = new LinkedHashMap<>();
         private int initialCount;
+        private int printLevel;
+
+        public StatsVisitor(int printLevel) {
+            this.printLevel = printLevel;
+        }
 
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
+            folderSizes.merge(file.getParent(), 0L, (old, n) -> old += attrs.size());
             return FileVisitResult.CONTINUE;
         }
 
@@ -34,6 +40,16 @@ public class FileWalker {
         public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(dir);
             Objects.requireNonNull(attrs);
+            if (initialPath == null) {
+                initialPath = dir;
+                initialCount = dir.getNameCount();
+            } else {
+                int relativeLevel = dir.getNameCount() - initialCount;
+                if (relativeLevel == 1) {
+                    folderSizes.clear();
+                }
+                folderSizes.put(dir, 0L);
+            }
             return FileVisitResult.CONTINUE;
         }
 
@@ -42,6 +58,21 @@ public class FileWalker {
             Objects.requireNonNull(dir);
 //            if (exc != null)
 //                throw exc;
+            if (dir.equals(initialPath)) {
+                return FileVisitResult.TERMINATE;
+            }
+            int relativeLevel = dir.getNameCount() - initialCount;
+            if (relativeLevel == 1) {
+                folderSizes.forEach((key, value) -> {
+                    int level = key.getNameCount() - initialCount - 1;
+                    if (level<printLevel){
+                    System.out.printf("%s[%s] - %,d bytes %n", "\t".repeat(level), key.getFileName(), value);
+                    }
+                });
+            } else {
+                long folderSize = folderSizes.get(dir);
+                folderSizes.merge(dir.getParent(), 0L, (old, n) -> old += folderSize);
+            }
             return FileVisitResult.CONTINUE;
         }
     }

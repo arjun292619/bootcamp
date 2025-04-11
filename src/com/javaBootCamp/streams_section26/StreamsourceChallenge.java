fce5dda5a67dc268c96874249d34c515736da8e6
arjun292619 <j.c.arjunreddy@gmail.com>
2025-04-11 01:43:38 -0700

    stream source challenge

--- src/com/javaBootCamp/streams_section26/StreamsourceChallenge.java
+++ src/com/javaBootCamp/streams_section26/StreamsourceChallenge.java
@@ -1,7 +1,16 @@
 package com.javaBootCamp.streams_section26;
 
+import java.util.stream.Stream;
+
 public class StreamsourceChallenge {
     public static void main(String[] args) {
+
+        int seed = 1;
+        Stream<String> streamB = Stream.iterate(seed, i -> i + 1)
+                .limit(15)
+                .map(i -> "B" + i);
+
+//        streamB.forEach(System.out::println);
 
     }
 }
